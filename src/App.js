788be042e7f1e68d01ed0783513e62f90f09b902
Ayush <ayushrock1010d.123@gmail.com>
2023-04-01 19:55:59 +0530

    NavBar changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default class App  extends Component {
 
@@ -10,8 +16,19 @@ export default class App  extends Component {
   render() {
     return (
       <div>
+        <Router>
        <NavBar/>
-       <News pageSize={5} category="sports" country ="us"/>
+       <Switch>
+          <Route path="/"><News pageSize={5} category="General" country ="us"/></Route>
+          <Route path="/Business"><News pageSize={5} category="Business" country ="us"/></Route>
+          <Route path="/Entertainment"><News pageSize={5} category="Entertainment" country ="us"/></Route>
+          <Route path="/General"><News pageSize={5} category="General" country ="us"/></Route>
+          <Route path="/Health"><News pageSize={5} category="Health" country ="us"/></Route>
+          <Route path="/Science"><News pageSize={5} category="Science" country ="us"/></Route>
+          <Route path="/Sports"><News pageSize={5} category="Sports" country ="us"/></Route>
+          <Route path="/Technology"><News pageSize={5} category="Technology" country ="us"/></Route>
+        </Switch>
+       </Router>
       </div>
     )
   }
