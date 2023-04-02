fe3dc6fc683491763f7a35e2f4a18dae2e8bb9b1
Ayush <ayushrock1010d.123@gmail.com>
2023-04-02 14:06:59 +0530

    navigation to particular sections

--- src/App.js
+++ src/App.js
@@ -19,14 +19,14 @@ export default class App  extends Component {
         <Router>
        <NavBar/>
        <Switch>
-          <Route path="/"><News pageSize={5} category="General" country ="us"/></Route>
-          <Route path="/Business"><News pageSize={5} category="Business" country ="us"/></Route>
-          <Route path="/Entertainment"><News pageSize={5} category="Entertainment" country ="us"/></Route>
-          <Route path="/General"><News pageSize={5} category="General" country ="us"/></Route>
-          <Route path="/Health"><News pageSize={5} category="Health" country ="us"/></Route>
-          <Route path="/Science"><News pageSize={5} category="Science" country ="us"/></Route>
-          <Route path="/Sports"><News pageSize={5} category="Sports" country ="us"/></Route>
-          <Route path="/Technology"><News pageSize={5} category="Technology" country ="us"/></Route>
+          <Route  exact path="/"><News pageSize={5} category="General" country ="us"/></Route>
+          <Route  exact path="/Business"><News pageSize={5} category="Business" country ="us"/></Route>
+          <Route  exact path="/Entertainment"><News pageSize={5} category="Entertainment" country ="us"/></Route>
+          <Route  exact path="/General"><News pageSize={5} category="General" country ="us"/></Route>
+          <Route  exact path="/Health"><News pageSize={5} category="Health" country ="us"/></Route>
+          <Route  exact path="/Science"><News pageSize={5} category="Science" country ="us"/></Route>
+          <Route  exact path="/Sports"><News pageSize={5} category="Sports" country ="us"/></Route>
+          <Route  exact path="/Technology"><News pageSize={5} category="Technology" country ="us"/></Route>
         </Switch>
        </Router>
       </div>
