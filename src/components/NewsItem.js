5a60dbf976c75b8c600e79432a64495c93443161
Ayush <ayushrock1010d.123@gmail.com>
2023-04-10 10:33:41 +0530

    NewsItem

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,11 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem;" }}>
-          <img src={imageUrl} class="card-img-top" alt="..." />
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}...<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            <h5 className="card-title">{title}...<span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
               {source}
-              <span class="visually-hidden">unread messages</span>
+              <span className="visually-hidden">unread messages</span>
             </span></h5>
             <p className="card-text">{description}...</p>
             <p className='card-text'><small className='text-muted'>By {!author ? "Unknown" : author} on {date}</small></p>
